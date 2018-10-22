9ac625f79b3af3bd28a1225415d2b649413db7d8
MINDOCK <parksy8189@gmail.com>
2018-10-23 01:55:35 +0900

    feat/report2: controller 작성
    1. GET / : 현재 시간 반환
    2. GET /users : 모든 회원 데이터 반환
    3. GET /users?name= : 같은 이름의 회원 데이터 반환
    		      없는 경우, '없습니다' 반환
    4. GET /users?part= : 같은 파트의 회원 데이터 반환
    		      없는 경우, '없습니다' 반환
    5. GET /users/{user_idx} : 해당 id의 회원 데이터 반환
    			   없는 경우, '없습니다' 반환
    6. POST /users : 회원 저장
    7. PUT /users/{user_idx} : 회원 정보 수정
    8. DELETE /users/{user_idx} : 회원 삭제

--- report2/src/main/java/org/sopt/report2/api/UserController.java
+++ report2/src/main/java/org/sopt/report2/api/UserController.java
@@ -1,7 +1,69 @@
 package org.sopt.report2.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.sopt.report2.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class UserController {
+
+    private static final String NOT_EXISTED = "없습니다";
+    private static final List<User> userList = new LinkedList<>();
+
+    @GetMapping("/")
+    public Date getNowDate() {
+        return new Date();
+    }
+
+    @GetMapping("/users")
+    public Object getUserList(@RequestParam(name = "name", required = false) String name,
+                              @RequestParam(name = "part", required = false) String part) {
+        if(name == null && part == null) {
+            return userList;
+        }
+        if(part == null) {
+            List<User> nameUsers = userList.stream().filter(user -> user.getName().equals(name)).collect(Collectors.toList());
+            if(nameUsers.size() > 0)
+                return nameUsers;
+            return NOT_EXISTED;
+        }
+        if(name == null) {
+            List<User> partUsers = userList.stream().filter(user -> user.getPart().equals(part)).collect(Collectors.toList());
+            if (partUsers.size() > 0)
+                return partUsers;
+            return NOT_EXISTED;
+        }
+        List<User> nameAndPartUsers = userList.stream().filter(user -> user.getName().equals(name) && user.getPart().equals(part)).collect(Collectors.toList());
+        if(nameAndPartUsers.size() > 0)
+            return nameAndPartUsers;
+        return NOT_EXISTED;
+    }
+
+    @GetMapping("/users/{user_idx}")
+    public Object getUserFromIdx(@PathVariable int user_idx) {
+        List<User> idxUser = userList.stream().filter(user -> user.getUser_idx() == user_idx).collect(Collectors.toList());
+        if(idxUser.size() > 0)
+            return idxUser.get(0);
+        return NOT_EXISTED;
+    }
+
+    @PostMapping("/users")
+    public void addUser(@RequestBody User user) {
+        userList.add(user);
+    }
+
+    @PutMapping("/users/{user_idx}")
+    public void updateUser(@PathVariable int user_idx, @RequestBody User inputUser) {
+        List<User> idxUser = userList.stream().filter(user -> user.getUser_idx() == user_idx).collect(Collectors.toList());
+        idxUser.get(0).update(inputUser);
+    }
+
+    @DeleteMapping("/users/{user_idx}")
+   public void deleteUser(@PathVariable int user_idx) {
+        userList.removeIf(user -> user.getUser_idx() == user_idx);
+    }
 }
--- report2/src/main/java/org/sopt/report2/model/User.java
+++ report2/src/main/java/org/sopt/report2/model/User.java
@@ -4,4 +4,21 @@ public class User {
     private int user_idx;
     private String name;
     private String part;
+
+    public int getUser_idx() {
+        return user_idx;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPart() {
+        return part;
+    }
+
+    public void update(User user) {
+        this.name = user.name;
+        this.part = user.part;
+    }
 }
