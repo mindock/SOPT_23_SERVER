af365acc93c84158cbf243132da46ce1e1adfdcc
MINDOCK <parksy8189@gmail.com>
2018-10-22 17:33:16 +0900

    feat/report2 : init report2 project

---
+++ report2/src/main/java/org/sopt/report2/api/UserController.java
@@ -0,0 +1,7 @@
+package org.sopt.report2.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ report2/src/main/java/org/sopt/report2/model/User.java
@@ -0,0 +1,7 @@
+package org.sopt.report2.model;
+
+public class User {
+    private int user_idx;
+    private String name;
+    private String part;
+}
